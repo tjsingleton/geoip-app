c0d200dfa48d4c27ac9eb2f48c5f715c16a9a2c1
TJ Singleton <tjsingleton@me.com>
2013-05-08 02:27:00 -0400

    support multiple ip addresses separated by +

--- lib/web_api.rb
+++ lib/web_api.rb
@@ -1,8 +1,10 @@
 require 'json'
+require 'resolv'
 
 class WebAPI
   DEFAULT_DB_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..', 'data', 'GeoLiteCity.dat'))
-  FOUR_OH_FOUR    = [404, {"Content-Type" => "application/json"}, StringIO.new(JSON.generate({error: "No known IP"}))]
+  FOUR_OH_FOUR    = [404, {"Content-Type" => "application/json"}, [JSON.generate({error: "No known IP"})]]
+  INVALID         = [400, {"Content-Type" => "application/json"}, [JSON.generate({error: "Invalid formatted IP"})]]
   COPYRIGHT_NOTICE = "This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com"
 
   def initialize(db_path = DEFAULT_DB_PATH)
@@ -10,15 +12,27 @@ class WebAPI
   end
 
   def call(env)
-    ip_address = File.basename(env["PATH_INFO"])
-    geoip      = Thread.current[:geoip] ||= GeoIP.new(@db_path)
+    geoip     = Thread.current[:geoip] ||= GeoIP.new(@db_path)
+    addresses = File.basename(env["PATH_INFO"]).split("+")
 
-    if struct = geoip.city(ip_address)
-      hash = Hash[struct.members.zip(struct.values)]
-      json = JSON.generate(hash)
-      [200, {"Content-Type" => "application/json", "X-Attribution" => COPYRIGHT_NOTICE}, [json]]
-    else
+    responses = addresses.map do |ip|
+      break(:invalid) unless Resolv::IPv4::Regex.match(ip)
+
+      if (geolocation = geoip.city(ip))
+        Hash[geolocation.members.zip(geolocation.values)]
+      end
+    end
+
+    if responses == :invalid
+      return INVALID
+    end
+
+    responses.compact!
+    if responses.empty?
       FOUR_OH_FOUR
+    else
+      json = JSON.generate({geolocation: responses, meta: {total: responses.count}})
+      [200, {"Content-Type" => "application/json", "X-Attribution" => COPYRIGHT_NOTICE}, [json]]
     end
   end
 end
--- spec/webapi_spec.rb
+++ spec/webapi_spec.rb
@@ -14,7 +14,23 @@ describe WebAPI do
     get "/api/lookup/174.36.207.186"
 
     last_response.status.should == 200
-    JSON.parse(last_response.body.to_s).should == {"request"=>"174.36.207.186", "ip"=>"174.36.207.186", "country_code2"=>"US", "country_code3"=>"USA", "country_name"=>"United States", "continent_code"=>"NA", "region_name"=>"DC", "city_name"=>"Washington", "postal_code"=>"", "latitude"=>38.89510000000001, "longitude"=>-77.0364, "dma_code"=>511, "area_code"=>202, "timezone"=>"America/New_York"}
+    JSON.parse(last_response.body.to_s).should == {
+        "geolocation" => [{"request"=>"174.36.207.186", "ip"=>"174.36.207.186", "country_code2"=>"US", "country_code3"=>"USA", "country_name"=>"United States", "continent_code"=>"NA", "region_name"=>"DC", "city_name"=>"Washington", "postal_code"=>"", "latitude"=>38.89510000000001, "longitude"=>-77.0364, "dma_code"=>511, "area_code"=>202, "timezone"=>"America/New_York"}],
+        "meta" => {"total" => 1}
+    }
+  end
+
+  it "looks up multiple ip addresses" do
+    get "/api/lookup/174.36.207.186+74.125.137.138"
+
+    last_response.status.should == 200
+    JSON.parse(last_response.body.to_s).should == {
+        "geolocation" => [
+            {"request"=>"174.36.207.186", "ip"=>"174.36.207.186", "country_code2"=>"US", "country_code3"=>"USA", "country_name"=>"United States", "continent_code"=>"NA", "region_name"=>"DC", "city_name"=>"Washington", "postal_code"=>"", "latitude"=>38.89510000000001, "longitude"=>-77.0364, "dma_code"=>511, "area_code"=>202, "timezone"=>"America/New_York"},
+            {"request"=>"74.125.137.138", "ip"=>"74.125.137.138", "country_code2"=>"US", "country_code3"=>"USA", "country_name"=>"United States", "continent_code"=>"NA", "region_name"=>"CA", "city_name"=>"Mountain View", "postal_code"=>"94043", "latitude"=>37.41919999999999, "longitude"=>-122.0574, "dma_code"=>807, "area_code"=>650, "timezone"=>"America/Los_Angeles"},
+        ],
+        "meta" => {"total" => 2}
+    }
   end
 
   it "404 when there is no data" do
@@ -22,5 +38,12 @@ describe WebAPI do
 
     last_response.status.should == 404
     JSON.parse(last_response.body.to_s).should == {"error" => "No known IP"}
+  end
+
+  it 'rejects invalid formatted ip addresses' do
+    get "/api/lookup/this-is-not-an-ip"
+
+    last_response.status.should == 400
+    JSON.parse(last_response.body.to_s).should == {"error" => "Invalid formatted IP"}
   end
 end
