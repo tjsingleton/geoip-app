85fbc3a318e0b27294d3b3b49a36928c0e3c53a1
TJ Singleton <tjsingleton@me.com>
2013-05-10 22:11:24 -0400

    javascript app for mapping ip addresses

---
+++ public/javascripts/geoip-app.js
@@ -0,0 +1,103 @@
+(function(){
+  var IPGeolocation = Backbone.Model.extend({
+    urlRoot: 'http://' + document.location.host + '/api/lookup',
+    parse: function(response, options){
+      return response.geolocation[0];
+    }
+  });
+
+  var IPGeolocationCollecton = Backbone.Collection.extend({
+    model: IPGeolocation
+  });
+
+  var AddressList =  Backbone.View.extend({
+    template: _.template("<% collection.each(function(ip) { %> <li><%= ip.id %></li> <% }); %>"),
+
+    initialize: function(options) {
+      this.listenTo(options.collection, "add", _.bind(this.render, this));
+    },
+
+    render: function(){
+      var src = this.template({collection: this.options.collection});
+      this.$el.html(src);
+    }
+  });
+
+  var NewAddressForm = Backbone.View.extend({
+    inputId: "ip-address",
+
+    events: {
+      'click button': 'addIP',
+      'submit': function(){ return false; }
+    },
+
+    initialize: function() {
+      _.bindAll(this, 'addIP');
+
+      this.input = document.getElementById(this.inputId);
+    },
+
+    addIP: function() {
+      var ip = this.input.value,
+          model = new IPGeolocation({id: ip}),
+          collection = this.options.collection;
+
+      this.input.value = "";
+
+      model.fetch({
+        success: function(){
+          collection.add(model);
+        },
+
+        error: function(){
+          alert("Could not add ip " + model.id +". Are you sure it's valid?");
+        }
+      });
+    }
+  });
+
+  var IPMap = function(mapId) {
+    this.map = new google.maps.Map(document.getElementById(mapId), {
+      center: new google.maps.LatLng(33.98, -83.69),
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      maxZoom: 18
+    });
+
+    this.mapBounds = new google.maps.LatLngBounds();
+  };
+
+  IPMap.prototype.addIP = function(model){
+    var lat = model.get('latitude'),
+        lng = model.get('longitude'),
+        coord, marker;
+
+    if (lat && lng) {
+      coord = new google.maps.LatLng(lat, lng);
+      this.mapBounds.extend(coord);
+      this.map.fitBounds(this.mapBounds);
+
+      marker = new google.maps.Marker({
+        position: coord,
+        map: this.map,
+        title: "IP: " + model.id
+      });
+    }
+  };
+
+  // Initialize
+  var addressCollection = new IPGeolocationCollecton();
+
+  var addressList = new AddressList({
+    el: document.getElementById('address-list'),
+    collection: addressCollection
+  });
+
+  var newAddress = new NewAddressForm({
+    el: document.getElementById('new-address'),
+    collection: addressCollection
+  });
+
+  var ipMap = new IPMap('map-canvas');
+  addressCollection.on("add", ipMap.addIP, ipMap);
+}());
